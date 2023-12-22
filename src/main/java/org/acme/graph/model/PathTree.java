b47259fb8df0c9031364bedf78e429c653b77518
Sivloc <clovis.bergeret@laposte.net>
2023-12-22 11:57:08 +0100

    Exo 9

--- src/main/java/org/acme/graph/model/PathTree.java
+++ src/main/java/org/acme/graph/model/PathTree.java
@@ -5,10 +5,11 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-
+ 
 import org.acme.graph.errors.NotFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.locationtech.jts.geom.Coordinate;
 
 public class PathTree {
 
@@ -31,7 +32,34 @@ public class PathTree {
 	public PathTree(Vertex origin) {
 		log.trace("initGraph{})", origin);
 		nodes = new HashMap<>();
+		Graph graph = new Graph();
+				
+		Vertex a = graph.createVertex(new Coordinate(0.0,0.0), "a");
+		graph.getVertices().add(a);
+
+		Vertex b = graph.createVertex(new Coordinate(1.0,0.0), "b");
+		graph.getVertices().add(b);
+
+		Vertex c = graph.createVertex(new Coordinate(2.0,0.0), "c");
+		graph.getVertices().add(c);
+
+		Vertex d = graph.createVertex(new Coordinate(1.0,1.0), "d");
+		graph.getVertices().add(d);
+
+		Edge ab = graph.createEdge(a, b, "ab");
+		graph.getEdges().add(ab);
+
+		Edge bc = graph.createEdge(b, c, "bc");
+		graph.getEdges().add(bc);
+
+		Edge ad = graph.createEdge(a, d, "ad");
+		graph.getEdges().add(ad);
 		
+		for (Vertex vertex : graph.getVertices()) {
+			PathNode pathnode = new PathNode(origin == vertex ? 0.0 : Double.POSITIVE_INFINITY, null, false);
+			nodes.put(vertex, pathnode);
+		}
+		this.origin = origin;
 	}
 
 	public Path getPath(Vertex destination) {
